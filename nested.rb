3bf1a3435bff33f82a818b04209cce1429a3e238
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-09-24 22:49:09 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -83,8 +83,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto][known_for: "Ruby", languages: ("LISP", "C")]
-programmer_hash[:yukihiro_matsumoto]
+programmer_hash[:yukihiro_matsumoto] = [known_for: "Ruby", languages: ["LISP", "C"]]
+programmer_hash
     
 end
 
@@ -105,9 +105,9 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     
-     
+     #alans_new_info = "GUI"
+programmer_hash[:alan_kay][:known_for] = "GUI"
+programmer_hash   
 end
 
 def adding_to_dennis
@@ -126,5 +126,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages]<< "Assembly"
+programmer_hash
 end
